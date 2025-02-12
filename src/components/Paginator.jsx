6b4f2a3d6e52426cf654a4a015c281dab4b2292b
Lucy Zhang <lululilly1212@tamu.edu>
2025-02-12 15:27:41 -0600

    testing libraries and configs set up

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -5,33 +5,34 @@ function Paginator({currPage, maxPages, onChange}) {
     const [page, setPage] = useState(currPage);
 
     const goToNextPage = () => {
-        if (page < maxPages) {
-          setPage((prev) => prev + 1);
-        }
+        try{
+            const p = parseInt(page);
+            if(!isNaN(p) && p < maxPages)
+                setPage(p + 1);
+        } catch {}
     };
 
     const goToPreviousPage = () => {
-        if (page > 1) {
-          setPage((prev) => prev - 1);
-        }
+        try{
+            const p = parseInt(page);
+            if(!isNaN(p) && p > 1)
+                setPage(p - 1);
+        } catch {}
     };
 
     useEffect(() => {
         try{
             const i = parseInt(page);
-            if(!isNaN(i))
+            if(!isNaN(i) && i > 1 && i <= maxPages)
                 onChange(i);
-        } catch{
-
-        }
-
+        } catch {}
     },[page])
 
     return (
         <div className="flex place-items-center h-fit">
             <button onClick={goToPreviousPage} className="mx-2 rounded-full"><FaAngleLeft /></button>
             <span className="mr-2">Page</span>
-            <input type="text" value={page} onChange={(event) => {setPage(event.target.value)}} className="p-0 m-0 w-6 text-center border-2 rounded-sm"></input>
+            <input type="text" value={page} onChange={(event) => {setPage(event.target.value)}} className="p-0 m-0 w-8 text-center border-2 rounded-sm"></input>
             <span className="ml-2">{` of ${maxPages ? maxPages : 0}`}</span>
             <button onClick={goToNextPage} className="mx-2 rounded-full"><FaAngleRight /></button>
         </div>
