2efea4b0fb966b6029ec6b4d2320ae43a06964a0
Stella Yang <stella@stellayang.dev>
2025-02-16 16:37:19 -0600

    jest set up to use babel transformer

---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.{js,jsx}'],
+    coverageDirectory: 'coverage',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    testMatch: ['**/tests/**/*.test.js', '**/tests/**/*.test.jsx'],
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+        "^.+\\.svg$": "jest-transformer-svg",
+    },    
+}
