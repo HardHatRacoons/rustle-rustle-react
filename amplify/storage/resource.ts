c36ee702e3f390c528b3c08239e81f5f53af55cd
Stella Yang <stella@stellayang.dev>
2025-02-24 17:25:39 -0600

    only allow authenticated to upload

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,10 +4,10 @@ export const storage = defineStorage({
   name: 'raccoonTeamDrive',
   access: (allow) => ({
       'annotated/*': [
-        allow.guest.to(['read']) // additional actions such as "write" and "delete" can be specified depending on your use case
+        allow.authenticated.to(['read']) // additional actions such as "write" and "delete" can be specified depending on your use case
       ],
       'unannotated/*': [
-        allow.guest.to(['read', 'write'])
+        allow.authenticated.to(['read', 'write'])
       ],
   })
 });
