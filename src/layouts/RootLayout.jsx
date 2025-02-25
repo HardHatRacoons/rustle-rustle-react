c21abdcd1a9ac4d538d1008ed6618f6b0da1638d
Stella Yang <stella@stellayang.dev>
2025-02-24 22:37:16 -0600

    center login menu

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,13 +3,15 @@ import {Outlet} from "react-router"
 
 function RootLayout() {
   return(
-      <Authenticator socialProviders={["google"]} hideSignUp>
+    <div className="h-screen bg-sky-300 place-content-center">
+         <Authenticator socialProviders={["google"]} hideSignUp>
           {({ signOut, user }) => (
               <div className="h-screen bg-sky-300">
                   <Outlet context={signOut}/>
               </div>
           )}
       </Authenticator>
+    </div>
   )
 }
 
