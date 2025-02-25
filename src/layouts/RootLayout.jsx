341c79aa9830b05af1175c62eb6b75d6f16c0d53
Stella Yang <stella@stellayang.dev>
2025-02-24 23:14:31 -0600

    restore rootlayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,22 +1,12 @@
 import { Authenticator } from '@aws-amplify/ui-react';
-import {Outlet} from "react-router"
-import GoogleSignIn from '../components/GoogleSignIn';
-import GoogleSignOut from '../components/GoogleSignOut';
+import { Outlet } from "react-router"
 
 function RootLayout() {
-  return(
-    <div className="h-screen bg-sky-300 place-content-center">
-        <GoogleSignIn />
-        <GoogleSignOut />
-        <Authenticator socialProviders={["google"]} hideSignUp>
-          {({ signOut, user }) => (
-              <div className="h-screen bg-sky-300">
-                  <Outlet context={signOut}/>
-              </div>
-          )}
-      </Authenticator>
-    </div>
-  )
+    return (
+        <div className="h-screen bg-sky-300">
+            <Outlet />
+        </div>
+    )
 }
 
 export default RootLayout
