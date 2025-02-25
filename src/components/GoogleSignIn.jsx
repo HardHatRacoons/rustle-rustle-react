b7d489324559e32ca9b8c1f5f0620abb96f07565
Emmie Teng <mteng@tamu.edu>
2025-02-24 23:32:43 -0600

    stylized home-made OAuth signin button

--- src/components/GoogleSignIn.jsx
+++ src/components/GoogleSignIn.jsx
@@ -1,11 +1,14 @@
-import { Authenticator } from '@aws-amplify/ui-react';
 import { signInWithRedirect } from 'aws-amplify/auth';
+import { FcGoogle } from "react-icons/fc";
+
 
 export default function LoginButton() {
     return (
         <div>
-            <button onClick={() => signInWithRedirect({ provider: "Google" })}>
-                Sign in with Google
+            <button className="flex flex-row content-center bg-white border-solid border-1 border-gray-500 px-5 py-3 gap-2 rounded-lg shadow-lg hover:bg-sky-100 " 
+                    onClick={() => signInWithRedirect({ provider: "Google" })}>
+                <FcGoogle size="30" /> 
+                <p className="font-bold color-gray-200 content-center">Sign In with Google</p>
             </button>
         </div>
     );
