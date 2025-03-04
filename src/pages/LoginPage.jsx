d8c931e7144863aa21db9bfc3daf2ce7691ce983
Lucy Zhang <lululilly1212@tamu.edu>
2025-03-04 14:40:38 -0600

    fix double login error

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,9 @@
-import { signInWithRedirect } from 'aws-amplify/auth';
+import { signInWithRedirect, signOut } from 'aws-amplify/auth';
 import { FcGoogle } from 'react-icons/fc';
+import { useUser } from '../components/UserContext';
 
 export default function LoginButton() {
+    const userAttributes = useUser();
     return (
         <div className="flex flex-col justify-center items-center h-screen">
             <div className="flex flex-col justify-center text-center gap-10 bg-white p-15 rounded-lg shadow-lg">
@@ -12,8 +14,10 @@ export default function LoginButton() {
                     <button
                         className="flex flex-row content-center bg-white border-solid border-1 border-gray-500 px-5 py-3 gap-2 rounded-lg shadow-lg hover:bg-sky-100 cursor-pointer"
                         aria-label="sign-in-button"
-                        onClick={() =>
-                            signInWithRedirect({ provider: 'Google' })
+                        onClick={async() =>
+                            {if(userAttributes !== null)
+                                await signOut();
+                            signInWithRedirect({ provider: 'Google' })}
                         }
                     >
                         <FcGoogle size="30" />
