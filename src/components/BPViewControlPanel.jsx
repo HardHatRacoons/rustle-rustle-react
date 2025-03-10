bc1e193eb02f6b46df0ad66505caa482110adfb1
Stella Yang <stella@stellayang.dev>
2025-03-10 03:38:15 -0500

    change pdfUrl to PDFInfo to pass more urls down

--- src/components/BPViewControlPanel.jsx
+++ src/components/BPViewControlPanel.jsx
@@ -1,15 +1,28 @@
 
-function BPViewControlPanel({ pdfURL }) {
+function BPViewControlPanel({ pdfInfo }) {
     return (
         <div className="shadow-lg rounded-md border-2 border-slate-400 flex flex-col h-full w-[300px]">
             <a
-                href={pdfURL}
+                href={pdfInfo.annotated_url}
                 target="_blank"
                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
             >
-                Download File
+                Download Annotated PDF
             </a>
-            
+            <a
+                href={pdfInfo.annotated_csv}
+                target="_blank"
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+                Download Extracted Data CSV
+            </a>
+            <a
+                href={pdfInfo.unannotated_url}
+                target="_blank"
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+                Download Unannotated PDF
+            </a>
         </div>
     );
 }
--- src/pages/BPView.jsx
+++ src/pages/BPView.jsx
@@ -3,12 +3,12 @@ import PDFViewer from '../components/PDFViewer';
 import { useOutletContext } from 'react-router';
 
 function BPView() {
-    const pdfURL = useOutletContext();
+    const pdfInfo = useOutletContext();
 
     return (
         <div className="select-none rounded-md border-solid border-2 border-sky-500 mx-2 mb-2 p-2 bg-white grow relative flex flex-row">
-            <PDFViewer pdfURL={pdfURL} />
-            <BPViewControlPanel pdfURL={pdfURL} />
+            <PDFViewer pdfURL={pdfInfo.annotated_url} />
+            <BPViewControlPanel pdfInfo={pdfInfo} />
         </div>
     );
 }
