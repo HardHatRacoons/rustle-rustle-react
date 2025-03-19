9808dc24b0e734111bd582d948e3f39d95af684c
Stella Yang <stella@stellayang.dev>
2025-03-19 01:41:28 -0500

    file layout testing

---
+++ tests/layoutTests/fileLayout.test.jsx
@@ -0,0 +1,158 @@
+import { MemoryRouter, Routes, Route } from 'react-router';
+import { render, screen, act } from '@testing-library/react';
+import FileLayout from '../../src/layouts/FileLayout'; // Adjust the import path as needed
+import { useUser } from '../../src/components/UserContext';
+import { vi } from 'vitest';
+import { getUrl, getProperties } from 'aws-amplify/storage';
+
+vi.mock('../../src/components/UserContext', () => {
+    const useUser = vi.fn();
+    return { useUser };
+})
+
+vi.mock('aws-amplify/storage', async (importOriginal) => {
+    const actual = await importOriginal();
+    return {
+        ...actual,
+        getUrl: vi.fn(),
+        getProperties: vi.fn(),
+    };
+})
+
+
+describe('FileLayout Component', () => {
+    afterEach(() => {
+        vi.restoreAllMocks();
+    });
+
+    it('shows loading when looking for data', async () => {
+        useUser.mockReturnValueOnce(null);
+        render(
+            <MemoryRouter initialEntries={['/child']}>
+                <Routes>
+                    <Route path="/" element={<FileLayout />}>
+                        <Route
+                            path="child"
+                            element={<p data-test-id="child-element">child</p>}
+                        />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        expect(document.body).toHaveTextContent('Loading...');
+    });
+
+    it('displays error if unannotated does not exist', async () => {
+        useUser.mockReturnValueOnce({ sub: 'testuser' });
+        getUrl.mockRejectedValueOnce(new Error('file not found'));
+        render(
+            <MemoryRouter initialEntries={['/child']}>
+                <Routes>
+                    <Route path="/" element={<FileLayout />}>
+                        <Route
+                            path="child"
+                            element={<p data-test-id="child-element">child</p>}
+                        />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        await act(async () => {});
+        expect(document.body).toHaveTextContent('Error');
+    });
+
+    it('displays error if annotated does not exist', async () => {
+        useUser.mockReturnValueOnce({ sub: 'testuser' });
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockRejectedValueOnce(new Error('file not found'));
+
+        render(
+            <MemoryRouter initialEntries={['/child']}>
+                <Routes>
+                    <Route path="/" element={<FileLayout />}>
+                        <Route
+                            path="child"
+                            element={<p data-test-id="child-element">child</p>}
+                        />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        await act(async () => {});
+        expect(document.body).toHaveTextContent('Error');
+    });
+
+    it('displays error if annotated csv data does not exist', async () => {
+        useUser.mockReturnValueOnce({ sub: 'testuser' });
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockRejectedValueOnce(new Error('file not found'));
+
+        render(
+            <MemoryRouter initialEntries={['/child']}>
+                <Routes>
+                    <Route path="/" element={<FileLayout />}>
+                        <Route
+                            path="child"
+                            element={<p data-test-id="child-element">child</p>}
+                        />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        await act(async () => {});
+        expect(document.body).toHaveTextContent('Error');
+    });
+
+    it('displays Document as a file name if the file metadata is empty', async () => {
+        useUser.mockReturnValueOnce({ sub: 'testuser' });
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getProperties.mockResolvedValueOnce({ metadata: {} });        
+
+        render(
+            <MemoryRouter initialEntries={['/child']}>
+                <Routes>
+                    <Route path="/" element={<FileLayout />}>
+                        <Route
+                            path="child"
+                            element={<p data-test-id="child-element">child</p>}
+                        />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        await act(async () => {});
+        expect(document.body).toHaveTextContent('Document');
+    });
+
+    it('displays Document as a file name if the file does not exist yet', async () => {
+        useUser.mockReturnValueOnce({ sub: 'testuser' });
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getUrl.mockReturnValueOnce({url: {toString: () => 'testurl'}});
+        getProperties.mockRejectedValueOnce(new Error('file not found'));        
+
+        render(
+            <MemoryRouter initialEntries={['/child']}>
+                <Routes>
+                    <Route path="/" element={<FileLayout />}>
+                        <Route
+                            path="child"
+                            element={<p data-test-id="child-element">child</p>}
+                        />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+        );
+
+        await act(async () => {});
+        expect(document.body).toHaveTextContent('Document');
+    });
+});
