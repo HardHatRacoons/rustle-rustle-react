30d837834b6e72a72bba16c2ca68d9424122bda3
Stella Yang <stella@stellayang.dev>
2025-03-23 23:58:40 -0500

    gray out button if any particular resource is unavailable

--- src/components/BPViewControlPanel.jsx
+++ src/components/BPViewControlPanel.jsx
@@ -1,11 +1,7 @@
-import { useEffect, useState } from 'react';
 import { LuDownload } from 'react-icons/lu';
 import { BsFiletypePdf, BsFiletypeCsv } from 'react-icons/bs';
 
 function BPViewControlPanel({ pdfInfo }) {
-    const isMissingDownloadLinks =
-        !pdfInfo.url.annotated.pdf || !pdfInfo.url.annotated.csv;
-
     return (
         <div className="shadow-lg rounded-md border-2 border-slate-400 flex flex-col h-full w-[300px] p-4">
             <p className="text-lg text-sky-900">Downloads:</p>
@@ -14,7 +10,7 @@ function BPViewControlPanel({ pdfInfo }) {
                     href={pdfInfo.url.annotated.pdf}
                     target="_blank"
                     className={`border-2 border-slate-400 rounded-md p-4 flex flex-col items-center justify-center col-span-2 
-                        ${isMissingDownloadLinks ? 'bg-gray-300' : 'bg-sky-300 hover:bg-sky-400'}`}
+                        ${!pdfInfo.url.annotated.pdf ? 'bg-gray-300' : 'bg-sky-300 hover:bg-sky-400'}`}
                 >
                     <LuDownload className="text-3xl mb-1" />
                     <span>Annotations</span>
@@ -23,7 +19,7 @@ function BPViewControlPanel({ pdfInfo }) {
                     href={pdfInfo.url.annotated.csv}
                     target="_blank"
                     className={`border-2 border-slate-400 rounded-md p-4 flex flex-col items-center justify-center 
-                        ${isMissingDownloadLinks ? 'bg-gray-200 text-white-700' : 'bg-green-200 hover:bg-green-30 text-gray-700'}`}
+                        ${!pdfInfo.url.annotated.csv ? 'bg-gray-200 text-white-700' : 'bg-green-200 hover:bg-green-30 text-gray-700'}`}
                 >
                     <BsFiletypeCsv className="text-3xl mb-1" />
                     <span>Data</span>
@@ -38,7 +34,7 @@ function BPViewControlPanel({ pdfInfo }) {
                     <span>Original</span>
                 </a>
             </div>
-            {isMissingDownloadLinks && (
+            {!pdfInfo.url.annotated.pdf && (
                 <p className="text-lg text-sky-900">
                     Blueprint currently processing
                 </p>
