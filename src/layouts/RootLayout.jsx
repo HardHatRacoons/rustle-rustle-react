7de51c63c966e2dc46c1e671a80273695eacaf3c
Emmie Teng <mteng@tamu.edu>
2025-03-30 14:59:04 -0500

    brought back gradient

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -16,7 +16,7 @@ function RootLayout() {
 
     return (
         <div
-            className={`h-screen bg-sky-200 dark:bg-slate-800 min-h-fit ${theme}`}
+            className={`h-screen bg-linear-to-t from-purple-300 to-sky-300 dark:from-slate-800 dark:to-slate-900 min-h-fit ${theme}`}
         >
             <Outlet context={[theme, setTheme]} />
         </div>
