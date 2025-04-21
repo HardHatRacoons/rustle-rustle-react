78dec956db2e655f70420f4c0bc09040e1820027
Emmie Teng <mteng@tamu.edu>
2025-04-21 16:14:35 -0500

    ran prettier

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,9 +9,10 @@ function Card({ children, idx, pin, onChange }) {
     return (
         <div className="w-full h-full shadow-lg rounded-md  text-sky-900 dark:text-slate-300 border-2 border-slate-100 dark:border-slate-700 dark:bg-slate-800 flex flex-col place-items-center">
             <div className="h-5 pt-5 p-5 w-full flex">
-                <div className="ml-auto my-auto hover:cursor-pointer hover:text-sky-700 dark:hover:text-slate-200"
-                     aria-label={'card-' + idx}
-                     onClick={onPin}
+                <div
+                    className="ml-auto my-auto hover:cursor-pointer hover:text-sky-700 dark:hover:text-slate-200"
+                    aria-label={'card-' + idx}
+                    onClick={onPin}
                 >
                     {pin ? <TbPinFilled size="30" /> : <TbPin size="30" />}
                 </div>
